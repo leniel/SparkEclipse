34fa5779d1e4be422986f21ae3d728a914d73038
Leniel Macaferi <leniel@gmail.com>
2016-12-10 18:46:55 -0200

    Added SparkFileUtils to rename\move the single .CSV file produced by Spark to its own folder to be consumed by spark-frontend.

--- spark-backend/src/main/scala/com/lenielmacaferi/spark/UsersXml.scala
+++ spark-backend/src/main/scala/com/lenielmacaferi/spark/UsersXml.scala
@@ -1,5 +1,11 @@
 package com.lenielmacaferi.spark
 
+/**
+ * @author Leniel Macaferi
+ *
+ * 12-2016
+ */
+
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.{ DataFrame, SQLContext }
 import com.databricks.spark.xml
@@ -19,7 +25,7 @@ object ProcessUsersXml {
   }
 
   def loadUsersData(sqlContext: SQLContext) = {
-    
+
     var df: DataFrame = null
 
     import sqlContext.implicits._
@@ -31,58 +37,58 @@ object ProcessUsersXml {
     df = sqlContext.read
       .format("xml")
       .option("rowTag", "user")
-      .load("data/Users10.out.xml") // For debugging purposes...
+      .load("input/Users10.out.xml") // For debugging purposes...
     //.load("data/Users100000.out.xml")// Reorganizing projects structure...
 
     // Displays the XML data structure
     df.printSchema()
-   
+
     df = convertToDateDf(df)
-    
+
     // Creating a TempView so that sqlContext can run queries against it
     df.createOrReplaceTempView("users")
-    
+
     //getAllUsers(sqlContext)
-    
+
     //getAllUsersOrderedByReputation(sqlContext)
-    
+
     //getUsersCountByLocation(sqlContext)
-    
+
     //searchAboutMe(sqlContext, "person")
-    
+
     getUsersGroupedByRegistrationYear(sqlContext)
-    
+
     getUsersGroupedByRegistrationMonthYear(sqlContext)
   }
-  
+
   def getAllUsers(sqlContext: SQLContext) = {
-    
+
     var df: DataFrame = sqlContext.sql("""SELECT * FROM users""")
-        
+
     df.show()
 
-    saveDfToJson(df, "AllUsers.json")
+    saveDfToCsv(df, "AllUsers.csv")
   }
-  
+
   def getUsersGroupedByRegistrationYear(sqlContext: SQLContext) = {
-    
+
     var df: DataFrame = sqlContext.sql("""SELECT YEAR(CD) as Year, COUNT(*) as Count FROM users GROUP BY YEAR(CD) ORDER BY Year""")
-        
+
     df.show()
 
-    saveDfToJson(df, "UsersGroupedByRegistrationYear.json")
+    saveDfToCsv(df, "UsersGroupedByRegistrationYear.csv")
   }
-  
-    def getUsersGroupedByRegistrationMonthYear(sqlContext: SQLContext) = {
-    
+
+  def getUsersGroupedByRegistrationMonthYear(sqlContext: SQLContext) = {
+
     // Functions available:
     // https://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/functions.html
     var df: DataFrame = sqlContext.sql("""SELECT DATE_FORMAT(CD, 'yyyy-MM') as MonthYear,
            COUNT(*) as Count FROM users GROUP BY DATE_FORMAT(CD, 'yyyy-MM') ORDER BY MonthYear""")
-        
+
     df.show()
 
-    saveDfToJson(df, "UsersGroupedByRegistrationMonthYear.json")
+    saveDfToCsv(df, "UsersGroupedByRegistrationMonthYear.csv")
   }
 
   def getAllUsersOrderedByReputation(sqlContext: SQLContext) = {
@@ -91,66 +97,65 @@ object ProcessUsersXml {
 
     df.show()
 
-    saveDfToJson(df, "AllUsersOrderedByReputation.json")
+    saveDfToCsv(df, "AllUsersOrderedByReputation.csv")
   }
-  
-    def getUsersCountByLocation(sqlContext: SQLContext) = {
-    
+
+  def getUsersCountByLocation(sqlContext: SQLContext) = {
+
     var df: DataFrame = sqlContext.sql("""SELECT Location, COUNT(*) AS Count FROM users GROUP BY Location""")
-        
+
     df.show()
 
-    saveDfToJson(df, "UsersCountByLocation.json")
+    saveDfToCsv(df, "UsersCountByLocation.csv")
   }
-    
- def searchAboutMe(sqlContext: SQLContext, search : String) = {
-    
+
+  def searchAboutMe(sqlContext: SQLContext, search: String) = {
+
     var df: DataFrame = sqlContext.sql("""SELECT DisplayName, AboutMe FROM users WHERE LOWER(AboutMe) LIKE '%person%'""")
-        
+
     df.show()
 
-    saveDfToJson(df, "SearchAboutMe.json")
+    saveDfToCsv(df, "SearchAboutMe.csv")
   }
 
-  def saveDfToJson(df: DataFrame, pathName: String) = {
+  def saveDfToCsv(df: DataFrame, fileName: String) = {
 
-    val fullPath: String = "data/"+pathName
-    
-    // Writing the query result to CSV for post processing in spark-frontend.
+    val fullPath: String = "output/" + fileName
+
+    // Writing the query result to CSV for post processing in Spreadsheet software for example
     df.coalesce(1) // Writes to a single file instead of creating multiple file partitions
       .write.option("header", "true") // Add headers to the CSV
       .mode("overwrite") // Overwrites the file if it already exists
-      .json("target/data/"+pathName) // Path where the CSV will be stored
+      .csv(fullPath) // Path where the CSV will be stored
+
+    SparkFileUtils.renameFile(fullPath, fileName)
   }
-   
-  def convertToDateDf(df: DataFrame) : DataFrame = {
-    
+
+  def convertToDateDf(df: DataFrame): DataFrame = {
+
     import org.apache.spark.sql.functions._
-    
-    val toDate = udf (
-        (entry : String) => {
-          
-          var retVal : java.sql.Date = null
-          
-          val sdf = new java.text.SimpleDateFormat("yyyy-MM-dd")
-          
-          try
-          {
-            retVal = new java.sql.Date(sdf.parse(entry).getTime)
+
+    val toDate = udf(
+      (entry: String) => {
+
+        var retVal: java.sql.Date = null
+
+        val sdf = new java.text.SimpleDateFormat("yyyy-MM-dd")
+
+        try {
+          retVal = new java.sql.Date(sdf.parse(entry).getTime)
+        } catch {
+          case e: Exception => {
+
+            retVal = null
           }
-           catch
-           {
-             case e: Exception => {
-               
-               retVal = null
-             }
-            }
-           
-           retVal
-        })
-        
-        df.withColumn("CD", toDate(df.col("CreationDate")))
-        //select("DisplayName", "CreationDate")
+        }
+
+        retVal
+      })
+
+    df.withColumn("CD", toDate(df.col("CreationDate")))
+    //select("DisplayName", "CreationDate")
   }
 
 }
